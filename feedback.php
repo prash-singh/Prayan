35273bd4e9022a62f35aae9292d7cd89d284259a
abhijain2000 <62430133+abhijain2000@users.noreply.github.com>
2021-11-29 01:35:22 +0530

    minor changes

--- feedback.php
+++ feedback.php
@@ -1,79 +1,112 @@
 <!DOCTYPE html>    
 <html>    
 <head>    
-<meta name="viewport" content="width=device-width, initial-scale=1">    
-<style>    
-* {    
-  box-sizing: border-box;    
-}    
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prayan</title>
+    <link rel="stylesheet" href="./feadback.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">    
+  <style>    
+  body {    
+    box-sizing: border-box;    
+  }    
+      
+  input[type=text], select, textarea {    
+    width: 100%;    
+    padding: 12px;    
+    border: 1px solid rgb(70, 68, 68);    
+    border-radius: 4px;    
+    resize: vertical;    
+  }    
+  input[type=email], select, textarea {    
+    width: 100%;    
+    padding: 12px;    
+    border: 1px solid rgb(70, 68, 68);    
+    border-radius: 4px;    
+    resize: vertical;    
+  }    
+      
+  label {    
+    padding: 12px 12px 12px 0;    
+    display: inline-block;    
+  }    
+      
+  input[type=submit] {    
+    background-color: rgb(37, 116, 161);    
+    color: white;    
+    padding: 12px 20px;    
+    border: none;    
+    border-radius: 4px;    
+    cursor: pointer;    
+    float: right;    
+  }    
+      
+  input[type=submit]:hover {    
+    background-color: #45a049;    
+  }    
+      
+  .container {    
+    border-radius: 5px;    
+    background-color: #f2f2f2;    
+    padding: 20px;    
+  }    
+      
+  .col-25 {    
+    float: left;    
+    width: 25%;    
+    margin-top: 6px;    
+  }    
+      
+  .col-75 {    
+    float: left;    
+    width: 75%;    
+    margin-top: 6px;    
+  }    
+  .row:after {    
+    content: "";    
+    display: table;    
+    clear: both;    
+  } 
+      
     
-input[type=text], select, textarea {    
-  width: 100%;    
-  padding: 12px;    
-  border: 1px solid rgb(70, 68, 68);    
-  border-radius: 4px;    
-  resize: vertical;    
-}    
-input[type=email], select, textarea {    
-  width: 100%;    
-  padding: 12px;    
-  border: 1px solid rgb(70, 68, 68);    
-  border-radius: 4px;    
-  resize: vertical;    
-}    
-    
-label {    
-  padding: 12px 12px 12px 0;    
-  display: inline-block;    
-}    
-    
-input[type=submit] {    
-  background-color: rgb(37, 116, 161);    
-  color: white;    
-  padding: 12px 20px;    
-  border: none;    
-  border-radius: 4px;    
-  cursor: pointer;    
-  float: right;    
-}    
-    
-input[type=submit]:hover {    
-  background-color: #45a049;    
-}    
-    
-.container {    
-  border-radius: 5px;    
-  background-color: #f2f2f2;    
-  padding: 20px;    
-}    
-    
-.col-25 {    
-  float: left;    
-  width: 25%;    
-  margin-top: 6px;    
-}    
-    
-.col-75 {    
-  float: left;    
-  width: 75%;    
-  margin-top: 6px;    
-}    
-    
-/* Clear floats after the columns */    
-.row:after {    
-  content: "";    
-  display: table;    
-  clear: both;    
-}    
-    
-/* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */    
-</style>    
+  </style>    
 </head> 
 
+<header>
+    <nav class="navbar">
+        <div class="navbar__top">
+            <div class="navbar__brand">
+                <a href="./index.php" rel="noopener noreferrer">
+                    <img src="./assets/logo.png" alt="logo" class="brand__logo">
+                </a>
+            </div>
+            <div class="navbar__nav__items">
+                <div class="nav__items">
+                    <h5 id="items__home"> <a href="./index.php">HOME</a></h5>
+                    <h5 id="items__about"> <a href="./about.html">ABOUT US</a></h5>
+                    <h5 id="items__offers"> <a href="./header/offer.html">OFFERS</a></h5>
+                    <h5 id="items__contact"><a href="./header/contact.html">Contact</a></h5>
+                </div>
+            </div>
+            <div class="navbar__account">
+                <div class="navbar__register">
+                    <h5 id="nav__login"> <a href="./login.html"> LOGIN</a></h5>
+                    <h4>|</h4>
+                    <h5 id="nav__register"> <a href="./signup.php">REGISTER</a></h5>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
+
 <body>    
-<h2>FEED BACK FORM</h2>    
+<h2 >FEEDBACK FORM</h2>    
 <div class="container">    
-  <form>    
+  <form >    
     <div class="row">    
       <div class="col-25">    
         <label for="fname">First Name</label>    
@@ -117,7 +150,7 @@ input[type=submit]:hover {
     </div>    
     <div class="row">    
       <div class="col-25">    
-        <label for="feed_back">Feed Back</label>    
+        <label for="feed_back">FeedBack</label>    
       </div>    
       <div class="col-75">    
         <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>    
@@ -128,7 +161,7 @@ input[type=submit]:hover {
     </div>    
   </form>    
 </div>    
-    
 </body> 
+
 
 </html> 
